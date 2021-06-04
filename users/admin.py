48be1c9d4caee89a11bd4cdf741db8ba30a4cabd
Paulina Torres <iptorresb25@gmail.com>
2021-06-03 18:43:30 -0600

    user admin config

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
+''' Users admin config '''
+
+#Django
 from django.contrib import admin
 
-# Register your models here.
+#Models
+from django.contrib.auth.models import User
+from user.models import Teacher, Student, Subject
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display=['id','teacher','title']
+    list_display_links=['id','teacher']
+    list_editable=['title']
+    search_fields=['teacher__email','teacher__is_staff','created_at','modified_at']
+    list_filter=['teacher__is_active','teacher__is_staff','created_at','modified_at']
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id','student','student_number','group']
+    list_display_links=['id','student']
+    list_editable=['group']
+    search_fields=['student__email','student__created']
+    list_filter=['student__is_active']
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display=['id','short_name','full_name','teacher']
+    list_display_links=['id']
+    search_fields=['short_name']
+    list_filter=['teacher']

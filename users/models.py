1563850df4d7175939efe09d2c01c1f63f7c6917
Paulina Torres <iptorresb25@gmail.com>
2021-06-03 18:20:15 -0600

    User models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
+''' Users Models '''
+# Django
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Teacher(models.Model):
+    teacher = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title', 'created_at']
+
+    def __str__(self):
+        return self.teacher.get_full_name()
+
+class Subject(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    short_name = models.CharField(max_length=10, blank=True)
+    full_name = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        ordering = ['short_name']
+
+    def __str__(self):
+        return self.short_name
+
+class Student(models.Model):
+    student = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_number = models.CharField(max_length=100, blank=True)
+    subject = models.ManyToManyField(Subject)
+    group = models.CharField(max_length=100, blank=True)
+
+    CAREERS = [
+        ("TI","TI"),
+        ("DN","DN"),
+        ("PI","PI")
+    ]
+
+    career = models.CharField(choices=CAREERS, max_length=50)
+
+    class Meta:
+        ordering = ['group','career','student_number']
+
+    def __str__(self):
+        return self.student.get_full_name()
